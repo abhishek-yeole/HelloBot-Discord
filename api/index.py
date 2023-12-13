d768088808e41562bfcd6e1c19739b21b06934e6
Abhishek Yeole <136891500+abhishek-yeole@users.noreply.github.com>
2023-12-13 23:53:45 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,50 @@
-from http.server import BaseHTTPRequestHandler
+import discord
+import mysql.connector
+import os
 
-class handler(BaseHTTPRequestHandler):
+mysql = mysql.connector.connect(
+    host=os.environ.get('DB_HOST'),
+    user=os.environ.get('DB_USER'),
+    password=os.environ.get('DB_PASSWORD'),
+    database=os.environ.get('DB_DATABASE'),
+    port=3306
+)
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+
+async def send_message(message, server_name):
+    try:
+        await message.channel.send(f'Hello World {server_name}')
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    cursor = mysql.cursor(dictionary=True)
+    cursor.execute("SELECT `token` FROM `auth_tokens` WHERE bot_id=%s", ((os.environ.get('BOT_ID')),))
+    bot_token = cursor.fetchone()
+    cursor.close()
+
+    TOKEN = bot_token['token']
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        server_name = message.author.guild.name
+
+        if client.user.mentioned_in(message):
+            await send_message(message, server_name)
+
+    client.run(TOKEN)
+
+
+if __name__ == '__main__':
+    run_discord_bot()
