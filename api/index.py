940fa20b264fe35d8a96dc63de2c15e8ad6cbdc9
Abhishek Yeole <136891500+abhishek-yeole@users.noreply.github.com>
2023-12-14 01:49:41 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,9 +2,6 @@ import discord
 from discord.ext import commands
 import mysql.connector
 import os
-from flask import Flask
-
-app = Flask(__name__)
 
 mysql = mysql.connector.connect(
     host=os.environ.get('DB_HOST'),
@@ -13,6 +10,7 @@ mysql = mysql.connector.connect(
     database=os.environ.get('DB_DATABASE'),
     port=3306
 )
+
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -41,13 +39,7 @@ async def send_message(message, server_name):
         print(e)
 
 
-# Flask route for health check or any other purposes
-@app.route('/')
-def home():
-    return "Discord Bot is running!"
-
-
-def run_discord_bot():
+if __name__ == '__main__':
     cursor = mysql.cursor(dictionary=True)
     cursor.execute("SELECT `token` FROM `auth_tokens` WHERE bot_id=%s", ((os.environ.get('BOT_ID')),))
     bot_token = cursor.fetchone()
@@ -56,11 +48,3 @@ def run_discord_bot():
     TOKEN = bot_token['token']
 
     bot.run(TOKEN)
-
-
-if __name__ == '__main__':
-    # Run the Flask application alongside the Discord bot
-    app.run(debug=True, host='0.0.0.0')
-
-    # Run the Discord bot
-    run_discord_bot()
