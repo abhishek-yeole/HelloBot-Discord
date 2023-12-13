6b3d3e04994f8d7a3064076352a876e5f8fb8ad2
Abhishek Yeole <136891500+abhishek-yeole@users.noreply.github.com>
2023-12-14 00:06:53 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -10,41 +10,48 @@ mysql = mysql.connector.connect(
     port=3306
 )
 
+from http.server import BaseHTTPRequestHandler
+ 
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world! Discord Bot'.encode('utf-8'))
+        run_discord_bot()
+        return
 
-async def send_message(message, server_name):
-    try:
-        await message.channel.send(f'Hello World {server_name}')
-    except Exception as e:
-        print(e)
-
-
-def run_discord_bot():
-    cursor = mysql.cursor(dictionary=True)
-    cursor.execute("SELECT `token` FROM `auth_tokens` WHERE bot_id=%s", ((os.environ.get('BOT_ID')),))
-    bot_token = cursor.fetchone()
-    cursor.close()
-
-    TOKEN = bot_token['token']
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = discord.Client(intents=intents)
-
-    @client.event
-    async def on_ready():
-        print(f'{client.user} is now running!')
-
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
-
-        server_name = message.author.guild.name
-
-        if client.user.mentioned_in(message):
-            await send_message(message, server_name)
-
-    client.run(TOKEN)
-
-
-if __name__ == '__main__':
-    run_discord_bot()
+    async def send_message(message, server_name):
+        try:
+            await message.channel.send(f'Hello World {server_name}')
+        except Exception as e:
+            print(e)
+    
+    
+    def run_discord_bot():
+        cursor = mysql.cursor(dictionary=True)
+        cursor.execute("SELECT `token` FROM `auth_tokens` WHERE bot_id=%s", ((os.environ.get('BOT_ID')),))
+        bot_token = cursor.fetchone()
+        cursor.close()
+    
+        TOKEN = bot_token['token']
+        intents = discord.Intents.default()
+        intents.message_content = True
+        client = discord.Client(intents=intents)
+    
+        @client.event
+        async def on_ready():
+            print(f'{client.user} is now running!')
+    
+        @client.event
+        async def on_message(message):
+            if message.author == client.user:
+                return
+    
+            server_name = message.author.guild.name
+    
+            if client.user.mentioned_in(message):
+                await send_message(message, server_name)
+    
+        client.run(TOKEN)
